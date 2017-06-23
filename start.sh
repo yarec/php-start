ddd6261ead21ffc798d23d6b0818e3484c45d638
yarec <yarec@163.com>
2017-06-24 06:44:10 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-docker run --name pstart --link=lnmp_mysql_1:db -p 81:80 yarec/php-start &
+ctrname=php-start
+#do=echo
+$do docker build -t yarec/$ctrname .
+$do docker stop $ctrname || true;
+$do docker rm $ctrname || true;
+$do docker run --name $ctrname -p 3000:80 --link=mysql:db -d yarec/$ctrname;
